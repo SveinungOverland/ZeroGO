810a5da846f18b6e39303c356167724b379f8399
Aleksander Johansen <aleksjoh@stud.ntnu.no>
2019-10-26 16:25:52 +0200

    Reworked buffer class for mcts

--- MonteCarlo/buffer.py
+++ MonteCarlo/buffer.py
@@ -1,10 +1,13 @@
 
-# data for each timestep is stored as (s_t,pi_t,z_t)
-# saves all moves to the win or loose
+# data for each timestep is stored as (s_t, pi_t, z_t)
+# saves all moves to the win or lose
 class Buffer():
     def __init__(self):
-        self.buffer = {}
+        self.data = []
 
-    #Save the state and policy-values
-    def remember_upper_conf(self,state, probabilites):
-        self.buffer[state] = probabilites
+        # Win = 1, tie = 0 and loss = -1.
+        self.result = None
+
+    # Save the state and policy-values
+    def remember_upper_conf(self, state, probabilites):
+        self.data.append((state, probabilites))
